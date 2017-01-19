98e3d011957f23e49a3e87b2d28fca7746cbd5f2
Antti Sand <antti.sand@tampere.fi>
2017-01-19 09:42:53 +0200

    Added promise plyfill for old ie

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -5,6 +5,8 @@ window.Vue = require('vue');
 window.axios = require('axios');
 window.Chart = require('chart.js');
 
+var Promise = require('es6-promise').Promise;
+
 window.axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest'
 };
