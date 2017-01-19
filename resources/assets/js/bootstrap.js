971d6c43f229e0766b42ba105d7656f442847231
Antti Sand <antti.sand@tampere.fi>
2017-01-19 09:48:40 +0200

    Added promise plyfill for old ie correctly?

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,11 +1,10 @@
 
-window._ = require('lodash');
+/*window._ = require('lodash');*/
 
-window.Vue = require('vue');
+/*window.Vue = require('vue');*/
+require('es6-promise').polyfill();
 window.axios = require('axios');
 window.Chart = require('chart.js');
-
-var Promise = require('es6-promise').Promise;
 
 window.axios.defaults.headers.common = {
     'X-Requested-With': 'XMLHttpRequest'
