b35a84dcfbc02e3e7741750a2f85eaba493f3a5c
Antti Sand <antti.sand@tampere.fi>
2017-01-19 09:37:18 +0200

    Downgrade to es5

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,7 @@ let map, listDiv = document.getElementById('list'), ctx = document.getElementByI
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 61.497594, lng: 23.759121}, //61.497594, 23.759121
+        center: {lat: 61.497594, lng: 23.759121},
         zoom: 13
     });
 
@@ -18,12 +18,12 @@ function showArrays(event) {
             window.data = response.data;
 
             let icdsString = '';
-            icds.data.forEach(item => {
+            icds.data.forEach(function(item) {
                 /*icdsString += '<a href="/chart/' + that.data.postinumero + '/' + item.icd + '/20160101">'+item.icd+'</a>, ';*/
                 icdsString += item.icd+', ';
             });
 
-            response.data.forEach(item => {
+            response.data.forEach(function(item) {
                 let genderString = (item.gender === 1) ? ' aikuisilla ' : ' lapsilla ';
 
                 let year = item.date.substr(0, 4);
@@ -41,35 +41,5 @@ function showArrays(event) {
                 '<p>'+ deceaseString+'</p>';
 
             listDiv.innerHTML = contentString;
-        }));
-
-    /*let data = axios.get('data/' + this.data.postinumero + '/20160101')
-        .then(response => {
-            window.data = response.data;
-            response.data.forEach(item => {
-
-                //deceaseString += 'ICD10: ' + item.icd + ' pvm: ' + item.date + ' lkm: ' + item.lkm + ' ryhma: ' + item.gender + '<br>';
-                let genderString = (item.gender === 1) ? ' aikuisilla ' : ' lapsilla ';
-                deceaseString += '<b>' + item.date + '</b><br> ICD10: ' + item.icd + '<br>'
-                    + genderString + item.lkm + ' tapausta<br>';
-            });
-
-            let contentString = '<b>Valittuna postinumeroalue ' + this.data.postinumero + '</b><br><br>' +
-                '<p>Alueen taudit:</p>' +
-                '<p>'+ deceaseString+'</p>';
-
-            /!*let myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                    datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                    }]
-                },
-            });*!/
-
-            listDiv.innerHTML = contentString;
-        })
-        .catch(error => console.log(error));*/
+        })).catch(function(error) {console.log(error)});
 }
